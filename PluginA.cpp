c01fd9c70f4d876554106b097861ef118e06594f
CaioRS <caiorss.rodrigues@gmail.com>
2019-03-12 11:16:40 -0300

    Remove unnused code.

--- PluginA.cpp
+++ PluginA.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cmath>
-
 #include <psdk/factory.hpp>
 
 class IMathFunction
--- main.cpp
+++ main.cpp
@@ -30,35 +30,5 @@ int main()
 	std::cout << "pLog->Name()    = " << pLog->Name() << "\n";
 	std::cout << "pLog->Eval(3.0) = " << pLog->Eval(3.0) << "\n";
 	
-	// Plugin pa{"PluginA"};
-	// using EntryPoint_t = IMathFunction* (*) (const char* className);
-		
-	// //auto factoryFunctionA = reinterpret_cast<EntryPoint_t>(pa.GetSymbol("factoryFunction");
-	// auto factoryFunctionA = pa.GetFunction<EntryPoint_t>("factoryFunction");
-	// assert(factoryFunctionA != nullptr);
-	
-
-	#if 0 
-	void* hnd = ::dlopen("PluginA.so", RTLD_LAZY);
-	assert(hnd != nullptr);
-
-    // Function pointer with signature of the plugin entry-point
-	// function (factory function).
-	using EntryPoint_t = IMathFunction* (*) (const char* className);
-		
-	auto factoryFunctionA = reinterpret_cast<EntryPoint_t>(::dlsym(hnd, "factoryFunction"));
-	assert(factoryFunctionA != nullptr);
-
-	IMathFunction* pExp = factoryFunctionA("Exp");
-	IMathFunction* pLog = factoryFunctionA("Log");
-	
-	std::cout << "pExp->Name()    = " << pExp->Name() << "\n";
-	std::cout << "pExp->Eval(3.0) = " << pExp->Eval(3.0) << "\n";
-
-	std::cout << "pLog->Name()    = " << pLog->Name() << "\n";
-	std::cout << "pLog->Eval(3.0) = " << pLog->Eval(3.0) << "\n";	   
-	::dlclose(hnd);
-	#endif 
-	
 	return 0;
 }
